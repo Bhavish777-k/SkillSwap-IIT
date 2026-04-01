6b11409265f89f331a4584a0c7f00a149f5611b8
KARNAVAT BHAVISH DIPAKBHAI <183609685+Bhavish777-k@users.noreply.github.com>
2026-04-01 07:01:45 +0530

    Final Done
    Done

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,35 @@
-import { defineConfig } from 'vite'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     port: 3000,
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:5000',
+//         changeOrigin: true,
+//       }
+//     }
+//   }
+// })
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
+export default defineConfig(({ mode }) => {
+  // Load env file based on current mode (development, production, etc.)
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      proxy: {
+        '/api': {
+          target: env.VITE_REACT_BACKEND_URL, // use the env variable
+          changeOrigin: true,
+        }
       }
     }
   }
